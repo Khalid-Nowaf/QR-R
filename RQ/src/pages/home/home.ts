120df8df66403bb28a68db9dfa0ad0cf6bb05457
KhalidNG <f5f@msn.com>
2017-03-28 12:52:07 -0700

    ...

--- RQ/src/pages/home/home.ts
+++ RQ/src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -10,18 +11,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   public data:any;
-  constructor(public navCtrl: NavController,private barcodeScanner: BarcodeScanner) {
-    
+  public lockup: {};
+  public pass:boolean = false;
+  constructor(public navCtrl: NavController,private barcodeScanner: BarcodeScanner,public toastCtrl: ToastController) {
+    this.lockup = {
+      "100001":true,
+      "100002":true,
+      "100003":false
+    };
  
   }
 
   public scan(){
        this.barcodeScanner.scan().then((barcodeData) => {
- this.data = JSON.stringify(barcodeData);
-}, (err) => {
-    this.data = err;
-});
-
-  }
+          this.data = JSON.stringify(barcodeData);
+              if(this.lockup[barcodeData.text]){
+                let toast = this.toastCtrl.create({
+                message: 'Good to go !! ' + barcodeData.text,
+                duration: 3000
+                });
+                toast.present();
+              }
+              else{
+                  let toast = this.toastCtrl.create({
+                  message: 'Not Good to go !! ' + barcodeData.text,
+                  duration: 3000
+                  });
+                toast.present();
+              }
+          }, (err) => {
+              this.data = err;
+      });
+    }
 
 }
